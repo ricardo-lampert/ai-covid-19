876ede4c4e51173d0e92a2ad9e4f04e212c5d89a
Ricardo Lampert <ricardo.lampert@softplan.com.br>
2022-03-13 16:07:10 -0300

    Update some functions and add features analysis

--- libs/feature_selection.py
+++ libs/feature_selection.py
@@ -8,3 +8,15 @@ def get_k_best_features(X, y, n_features=10):
     fit = k_best_classifier.fit(X, y)
     cols = fit.get_support(indices=True)
     return X.iloc[:, cols]
+
+def fs_results_filter(fs_results,n_features):
+    fs_filtered_results = []
+    ultimo = 0
+    for feature,times in fs_results.items():
+        if  n_features>0 or times==ultimo:
+            fs_filtered_results.append(feature)
+            n_features-=1
+            if n_features == 0:
+                ultimo = times
+        else:
+            return fs_filtered_results
