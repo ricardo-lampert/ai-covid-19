da8b25e4d3758bd6447467d9e95a245e3363d891
Ricardo Lampert <ricardo.lampert@softplan.com.br>
2022-03-15 11:52:32 -0300

    Add new feature selection models

--- libs/feature_selection.py
+++ libs/feature_selection.py
@@ -1,5 +1,9 @@
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif, mutual_info_classif, chi2
+from sklearn.ensemble import ExtraTreesClassifier
+
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
 
 # f_classif
 # ANOVA F-value between label/feature for classification tasks.
@@ -14,56 +18,54 @@ def get_k_best(X, y, classif, n_features=10):
         k_best_classifier = SelectKBest(score_func=f_classif, k=n_features)
     elif classif == "mutual_info_classif":
         k_best_classifier = SelectKBest(score_func=mutual_info_classif, k=n_features)
-    else :
+    else:
         k_best_classifier = SelectKBest(score_func=chi2, k=n_features)
-    
+
     fit = k_best_classifier.fit(X, y)
     cols = fit.get_support(indices=True)
-    return X.iloc[:, cols]
+    return X.iloc[:, cols].columns.to_list()
 
 
-
-# from sklearn.feature_selection import RFE
-# from sklearn.linear_model import LogisticRegression
-# model = LogisticRegression(max_iter=5000)
-# selector = RFE(model, N_FEATURES)
-# features = selector.fit(X_raw, y_raw)
-# scores=[]
-# for i,score in enumerate(features.ranking_):
-#   scores.append((data.feature_names[i],score))
-# scores.sort(key=lambda x: x[1])
-# for i in range(N_FEATURES):
-#   print(scores[i][0])
-# print("\nObs: Desconsiderar a ordem")
-# X = features.transform(X_raw)
-# y = y_raw
-
-# from sklearn.ensemble import ExtraTreesClassifier
-# model = ExtraTreesClassifier(n_estimators=N_FEATURES)
-# model.fit(X_raw, y_raw)
-# # print(model.feature_importances_)
-# scores=[]
-# features=[]
-# for i,score in enumerate(model.feature_importances_):
-#   scores.append((data.feature_names[i],score))
-  
-# scores.sort(key=lambda x: x[1],reverse=True)
-# for i in range(N_FEATURES):
-#   print(scores[i][0])
-#   features.append(data.feature_names[i])
-# X = df[features]
-# y = y_raw
+# Feature importance
+def get_extra_trees_classifier(X, y, data, n_features=10):
+    model = ExtraTreesClassifier(n_estimators=n_features)
+    model.fit(X, y)
+    scores = []
+    features = []
+    for i, score in enumerate(model.feature_importances_):
+        scores.append((data.columns[i], score))
+    scores.sort(key=lambda x: x[1], reverse=True)
+    for i in range(n_features):
+        features.append(scores[i][0])
+    return features
 
 
-def fs_results_filter(fs_results,n_features):
+# RFE
+def fs_results_filter(fs_results, n_features):
     fs_filtered_results = []
     ultimo = 0
-    for feature,times in fs_results.items():
-        if  n_features>0 or times==ultimo:
+    for feature, times in fs_results.items():
+        if n_features > 0 or times == ultimo:
             fs_filtered_results.append(feature)
-            n_features-=1
+            n_features -= 1
             if n_features == 0:
                 ultimo = times
         else:
             return fs_filtered_results
     return fs_filtered_results
+
+
+def get_rfe(X, y, data, n_features):
+    model = LogisticRegression(max_iter=5000)
+    selector = RFE(estimator=model, n_features_to_select=n_features)
+    features = selector.fit(X, y)
+    scores = []
+    best_features = []
+    for i, score in enumerate(features.ranking_):
+        scores.append((data.columns[i], score))
+    scores.sort(key=lambda x: x[1])
+    for i in range(n_features):
+        # print(scores[i][0])
+        best_features.append(scores[i][0])
+    # print("\nObs: Desconsiderar a ordem\n")
+    return best_features
